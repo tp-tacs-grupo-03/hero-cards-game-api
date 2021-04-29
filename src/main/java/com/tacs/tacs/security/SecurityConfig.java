74f434aba01b9837a85524d51b120f1d122bafea
nico9813 <nico.gomez.mbc@gmail.com>
2021-04-29 13:18:18 -0300

    Autorizacion a todas las rutas excepto swagger

---
+++ src/main/java/com/tacs/tacs/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.tacs.tacs.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+        // -- Swagger UI v2
+        "/v2/api-docs",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/configuration/ui",
+        "/configuration/security",
+        "/swagger-ui.html",
+        "/webjars/**",
+        // -- Swagger UI v3 (OpenAPI)
+        "/v3/api-docs/**",
+        "/swagger-ui/**"
+    };
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+      .antMatchers(AUTH_WHITELIST).permitAll()
+      .anyRequest()
+      .authenticated()
+      .and()
+      .oauth2ResourceServer()
+      .jwt();
+  }
+}
