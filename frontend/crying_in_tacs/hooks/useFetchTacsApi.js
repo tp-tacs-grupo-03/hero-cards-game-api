465d5b1ad2943276d8346c4965bd4d8de6ee1fb6
nico9813 <nico.gomez.mbc@gmail.com>
2021-05-01 13:21:08 -0300

    Agrego fetch con autorizacion

---
+++ frontend/crying_in_tacs/hooks/useFetchTacsApi.js
@@ -0,0 +1,38 @@
+import { useAuth0 } from "@auth0/auth0-react"
+
+var prevAccessToken
+
+export function useFetchTacsApi() {
+
+    const audience = `https://tacs.2021.com`
+
+    const { getAccessTokenSilently } = useAuth0()
+
+    return async function fetchPrelloApi(path, method, body = {}) {
+
+        const accessToken = prevAccessToken ?? await getAccessTokenSilently({
+            audience: audience
+        })
+
+        prevAccessToken = accessToken
+
+        const httpbody = method != 'GET' ? { body: JSON.stringify(body) } : {}
+        const httppath = `http://localhost:8080/api/${path}`
+
+        try {
+            const options = {
+                method: method,
+                headers: {
+                    'Content-Type': "application/json",
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                ...httpbody
+            }
+
+            const data = await fetch(httppath, options)
+            return await data.json()
+        } catch (e) {
+            console.log(e.message)
+        }
+    }
+}
