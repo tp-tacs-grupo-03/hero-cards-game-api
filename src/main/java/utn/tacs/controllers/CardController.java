5f91daccbc57b63665230d5de40e8f8aa1506b72
Federico Medina <femedina@prismamp.com>
2021-05-11 19:40:37 -0300

    Cambio las firmas

--- src/main/java/utn/tacs/controllers/CardController.java
+++ src/main/java/utn/tacs/controllers/CardController.java
@@ -4,20 +4,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.springframework.lang.NonNull;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import utn.tacs.dto.card.CardModelResponse;
 import utn.tacs.dto.card.CardFindRequest;
-import utn.tacs.dto.deck.DeckAddRequest;
-import utn.tacs.dto.deck.DeckCardDeleteRequest;
 import utn.tacs.services.CardFinder;
 import utn.tacs.domain.CardId;
-import utn.tacs.services.DecksCardCleaner;
-import utn.tacs.services.DecksCardCreator;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RequestMapping("api/cards")
 @Api(tags = "Cards")
@@ -25,14 +17,10 @@ import java.util.stream.Collectors;
 @RestController
 public class CardController {
 
-    private CardFinder finder;
-    private DecksCardCleaner decksCardCleaner;
-    private DecksCardCreator decksCardCreator;
+    private final CardFinder finder;
 
-    public CardController(CardFinder finder, DecksCardCleaner decksCardCleaner, DecksCardCreator decksCardCreator){
-        this.decksCardCleaner = decksCardCleaner;
+    public CardController(CardFinder finder){
         this.finder = finder;
-        this.decksCardCreator = decksCardCreator;
     }
 
     @GetMapping("/{id}")
@@ -44,21 +32,4 @@ public class CardController {
         return finder.find(new CardFindRequest(new CardId(id)));
     }
 
-    @DeleteMapping("/{id}/cards")
-    @ApiOperation(value = "Quita una carta al deck del id")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Deck con la carta nueva")
-    })
-    public void deleteCard(@PathVariable("id") String id, @RequestBody CardId cardId) throws Exception {
-        decksCardCleaner.delete(new DeckCardDeleteRequest(id, cardId));
-    }
-
-    @PatchMapping("/{id}/cards")
-    @ApiOperation(value = "Agregar cartas al deck del id")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Deck con la carta nueva")
-    })
-    public void addCard(@PathVariable("id") String id, @Validated @NonNull @RequestBody List<String> cardIds) throws Exception {
-        decksCardCreator.add(new DeckAddRequest(id, cardIds.stream().map(CardId::new).collect(Collectors.toList())));
-    }
 }
