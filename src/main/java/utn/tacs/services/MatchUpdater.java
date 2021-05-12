aec9ee1770c8d9fff9f194c703082d01e7f42f30
Federico Medina <femedina@prismamp.com>
2021-05-11 23:43:54 -0300

    Agrego etiqueta de service

--- src/main/java/utn/tacs/services/MatchUpdater.java
+++ src/main/java/utn/tacs/services/MatchUpdater.java
@@ -1,11 +1,17 @@
 package utn.tacs.services;
 
+import org.springframework.stereotype.Service;
 import utn.tacs.domain.Match;
 import utn.tacs.dto.match.MatchUpdateRequest;
 import utn.tacs.repositories.MatchesRepository;
 
+@Service
 public class MatchUpdater {
     MatchesRepository repository;
+
+    public MatchUpdater(MatchesRepository repository){
+        this.repository = repository;
+    }
 
     public void update(MatchUpdateRequest matchUpdateRequest) throws Exception {
         Match match = repository.find(matchUpdateRequest.getId()).orElseThrow(()-> new Exception("No hay match con ese id"));
