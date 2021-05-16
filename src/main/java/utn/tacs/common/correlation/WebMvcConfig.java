cd5b15e1a60fa11d00c4aa189c9444803113398d
Federico Medina <femedina@prismamp.com>
2021-05-16 17:37:42 -0300

    Agrego eTag

--- src/main/java/utn/tacs/common/correlation/WebMvcConfig.java
+++ src/main/java/utn/tacs/common/correlation/WebMvcConfig.java
@@ -1,6 +1,9 @@
 package utn.tacs.common.correlation;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +13,14 @@ public class WebMvcConfig  implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RestHeaderInterceptor());
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+                = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/api/*");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
     }
 }
