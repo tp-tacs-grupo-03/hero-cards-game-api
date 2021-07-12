956c1a239e07c8d0e3d324e5752fb88d5cc6fb6b
FredeHG <federicom503@gmail.com>
2021-07-12 02:09:58 -0300

    fix test

--- src/test/java/utn/tacs/controllers/PlayerControllerTest.java
+++ src/test/java/utn/tacs/controllers/PlayerControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import utn.tacs.TacsApplication;
 import utn.tacs.common.security.Authenticator;
 import utn.tacs.domain.PlayerStats;
@@ -25,7 +26,7 @@ final public class PlayerControllerTest extends RequestTestCase{
     @Test
     void getAllPlayers() throws Exception {
         Mockito.when(authenticator.getHost()).thenReturn("test");
-        Mockito.when(usersRepository.findByName("", new Sort("name", "asc"))).thenReturn(new ArrayList<>());
+        Mockito.when(usersRepository.findByName( PageRequest.of(0, 100), "")).thenReturn(new ArrayList<>());
         assertRequest("GET", "/api/players", 200);
     }
 
