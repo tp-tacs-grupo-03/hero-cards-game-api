fc3c8f2b9b9f262e04ccd3bcaefd642bfd729ab5
FredeHG <federicom503@gmail.com>
2021-07-12 02:08:09 -0300

    Match test

---
+++ src/test/java/utn/tacs/controllers/MatchControllerTest.java
@@ -0,0 +1,29 @@
+package utn.tacs.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatchControllerTest {
+
+    @Test
+    void getAllMatches() {
+    }
+
+    @Test
+    void getMatch() {
+    }
+
+    @Test
+    void postMatch() {
+    }
+
+    @Test
+    void surrender() {
+    }
+
+    @Test
+    void playGame() {
+
+    }
+}
